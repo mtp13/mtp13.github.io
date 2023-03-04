58334efd572f8beea720bb1f442a24315bb717e7
Mike Pullen <mike.pullen@gmail.com>
2023-03-04 16:30:27 -0600

    initial working version

--- clock/clock.js
+++ clock/clock.js
@@ -1,4 +1,23 @@
-function now() {
-  let now = Date.now();
-  return now;
+
+function updateClock() {
+  const date = new Date();
+  let [hour, minutes, seconds] = [
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+  ];
+
+  let timeOfDay = (hour >= 12) ? "PM" : "AM";
+  if (hour === 0) {
+    hour = 12;
+  } else {
+    hour = (hour > 12) ? (hour -= 12) : hour;
+  } 
+
+  minutes = (minutes > 9) ? minutes : `0${minutes}`;
+  seconds = (seconds > 9) ? seconds : `0${seconds}`;
+
+  let time = `${hour}:${minutes}:${seconds} ${timeOfDay}`;
+  document.getElementById("time").innerHTML = time;
+
 }
