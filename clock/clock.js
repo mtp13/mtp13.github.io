c7d726d0b2a94449c1a64130c12465b8f4e1edc8
Mike Pullen <mike.pullen@gmail.com>
2023-03-05 13:06:36 -0600

    refactor

--- clock/clock.js
+++ clock/clock.js
@@ -1,23 +1,25 @@
 
 function updateClock() {
   const date = new Date();
+  // const date = new Date("1995-12-17T23:24:00");
   let [hour, minutes, seconds] = [
     date.getHours(),
     date.getMinutes(),
     date.getSeconds(),
   ];
 
-  let timeOfDay = (hour >= 12) ? "PM" : "AM";
+  let timeOfDay = (hour < 12) ? "AM" : "PM";
   if (hour === 0) {
     hour = 12;
   } else {
-    hour = (hour > 12) ? (hour -= 12) : hour;
-  } 
+    hour = (hour <= 12) ? hour : (hour -= 12);
+  }
 
-  minutes = (minutes > 9) ? minutes : `0${minutes}`;
-  seconds = (seconds > 9) ? seconds : `0${seconds}`;
+  minutes = (minutes >= 10) ? minutes : `0${minutes}`;
+  seconds = (seconds >= 10) ? seconds : `0${seconds}`;
 
   let time = `${hour}:${minutes}:${seconds} ${timeOfDay}`;
   document.getElementById("time").innerHTML = time;
-
 }
+
+setInterval('updateClock()', 500)
