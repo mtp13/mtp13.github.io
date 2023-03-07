8754d96831233801bf32c76523e4c0506b4819ed
Mike Pullen <mike.pullen@gmail.com>
2023-03-07 15:09:43 -0600

    use displayOptions.  use built-in date methods to display time. and some css tweaks.

--- clock/clock.js
+++ clock/clock.js
@@ -1,40 +1,21 @@
-function zeroPad(numberStr) {
-  return numberStr.padStart(2, "0");
-}
-
 function updateClock() {
-  const TWELVE = "12Hour";
-  let timeOfDay ="";
-  const date = new Date();   // const date = new Date("1995-12-17T23:24:00");
-  let [hour, minutes, seconds] = [
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds(),
-  ];
-  
-  const timeFormat = document.querySelector('input[name="timeFormat"]:checked').value;
-  if (timeFormat === TWELVE) {
-    timeOfDay = (hour < 12) ? "AM" : "PM";
-    if (hour === 0) {
-      hour = 12;
-    } else {
-      hour = (hour <= 12) ? hour : (hour -= 12);
-    }
+  const date = new Date();
+  const militaryTime = document.getElementById("military");
+  let clock;
+
+  if (militaryTime.checked) {
+    clock = date.toTimeString().replace(/ GMT.*/, "");
+  } else {
+    clock = date.toLocaleTimeString();
   }
-
-  minutes = zeroPad(minutes.toString());
-  seconds = zeroPad(seconds.toString());
-
-  let clock = `${hour}:${minutes}:${seconds} ${timeOfDay}`;
   document.getElementById("clock").innerHTML = clock;
 
-  const showDate = document.querySelector('input[name="showDate"]:checked');
-  if (showDate) {
+  const showDate = document.getElementById("showDate");
+  if (showDate.checked) {
     document.getElementById("date").innerHTML = date.toDateString();
   } else {
-    document.getElementById("date").innerHTML = " ";
+    document.getElementById("date").innerHTML = "";
   }
-
 }
 
 setInterval('updateClock()', 500)
